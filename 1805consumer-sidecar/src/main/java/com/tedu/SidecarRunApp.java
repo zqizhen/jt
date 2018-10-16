93b734e038cd47eb091bfd465b01a385d0ffa94e
zqizhen <845345738@qq.com>
2018-10-16 11:49:22 +0800

    jt03
    Signed-off-by: zqizhen <845345738@qq.com>

---
+++ 1805consumer-sidecar/src/main/java/com/tedu/SidecarRunApp.java
@@ -0,0 +1,22 @@
+package com.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableSidecar
+public class SidecarRunApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidecarRunApp.class, args);
+	}
+}
